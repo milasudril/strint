90e6f873a13e3abc1ba30e46ec8573d0c2af471d
Torbjörn Rathsman <milasudril@gmail.com>
2018-04-05 20:04:22 +0200

    Move IntBse to separate file

---
+++ int_base.hpp
@@ -0,0 +1,40 @@
+//@	{"targets":[{"name":"int_base.hpp","type":"include"}]}
+
+#ifndef TYPE_INTBASE_H
+#define TYPE_INTBASE_H
+
+#include "narrow_cast.hpp"
+
+namespace Type
+	{
+	template<class IntegerType>
+	class IntBase
+		{
+		public:
+			template<class U,std::enable_if_t<IsLosslessConvertible<U,IntegerType>::value,int> x=1>
+			constexpr IntBase(U value) noexcept:m_value(value)
+				{}
+
+			template<class U,std::enable_if_t<!IsLosslessConvertible<U,IntegerType>::value,int> x=1>
+			constexpr explicit IntBase(U value):m_value(narrow_cast<IntegerType>(value))
+				{}
+
+			explicit constexpr operator IntegerType() const noexcept
+				{return m_value;}
+
+			static constexpr bool isUnsigned() noexcept
+				{return IsUnsigned<IntegerType>::value;}
+
+			static constexpr bool isSigned() noexcept
+				{return !isUnsigned();}
+
+		protected:
+			IntegerType m_value;
+		};
+
+	template<class IntegerType>
+	struct IsUnsigned<IntBase<IntegerType>>
+		{static constexpr bool value = IntBase<IntegerType>::isUnsigned();};
+	}
+
+#endif
--- integers.hpp
+++ integers.hpp
@@ -3,41 +3,14 @@
 #ifndef TYPE_INTEGERS_HPP
 #define TYPE_INTEGERS_HPP
 
+#include "int_base.hpp"
+
 #include "narrow_cast.hpp"
 #include <cstdint>
 #include <climits>
 
 namespace Type
 	{
-	template<class IntegerType>
-	class IntBase
-		{
-		public:
-			template<class U,std::enable_if_t<IsLosslessConvertible<U,IntegerType>::value,int> x=1>
-			constexpr IntBase(U value) noexcept:m_value(value)
-				{}
-
-			template<class U,std::enable_if_t<!IsLosslessConvertible<U,IntegerType>::value,int> x=1>
-			constexpr explicit IntBase(U value):m_value(narrow_cast<IntegerType>(value))
-				{}
-
-			explicit constexpr operator IntegerType() const noexcept
-				{return m_value;}
-
-			static constexpr bool isUnsigned() noexcept
-				{return IsUnsigned<IntegerType>::value;}
-
-			static constexpr bool isSigned() noexcept
-				{return !isUnsigned();}
-
-		protected:
-			IntegerType m_value;
-		};
-
-	template<class IntegerType>
-	struct IsUnsigned<IntBase<IntegerType>>
-		{static constexpr bool value = IntBase<IntegerType>::isUnsigned();};
-
 	enum class IntSize:int
 		{
 		 Smallest = CHAR_BIT
